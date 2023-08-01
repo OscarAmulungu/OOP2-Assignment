38ae9d4daed0c398c32e7e39c03b5a283df4aea3
Oscar Amulungu <110774590+OscarAmulungu@users.noreply.github.com>
2023-08-02 00:42:16 +0200

    Update Samsung.java

--- Samsung.java
+++ Samsung.java
@@ -3,16 +3,16 @@ package Company;
 //Inheritance
 public class Samsung extends Device{
 
-	public Samsung(String nN, Double nP, String nC, String nB, Integer nY, Integer nS) {
-		super(nN, nP, nC, nB, nY, nS);
+	public Samsung(String nN, Double nP, String nC, String nB, Integer nY, Double nDS) {
+		super(nN, nP, nC, nB, nY, nDS);
 		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public Double SellingPrice() {
-		return Price + Price*0.5;
+		return Price + Price*0.4;
 	}
 	@Override
 	public Double DiscountPrice(){
-		return Price - 400;
+		return SellingPrice() - 1100;
 	}
 }
