4094105eb73a8354fad98673570febcc2a939d34
NamwenyoW <110904685+Namwenyo@users.noreply.github.com>
2023-08-10 01:18:51 +0200

    Update Device.java

--- Device.java
+++ Device.java
@@ -2,14 +2,14 @@ package Company;
 
 public abstract class Device {
 	protected String Name;
-	protected Double Price;
+	protected Double BasicPrice;
 	protected String Color;
 	protected String Brand;
 	protected int Year;
 	protected Double DisplaySize;
 	public Device(String nN, Double nP, String nC, String nB, int nY, Double nDS) {
 		this.Name = nN;
-		this.Price = nP;
+		this.BasicPricePrice = nP;
 		this.Color = nC;
 		this.Brand = nB;
 		this.Year = nY;
@@ -19,8 +19,8 @@ public abstract class Device {
 	public String GetName(){
 		return Name;
 	}
-	public Double GetPrice(){
-		return Price;
+	public Double GetBasicPrice(){
+		return BasicPrice;
 	}
 	public String GetColor(){
 		return Color;
