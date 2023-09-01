f2500480397ac862733992c99ac97f650eb23659
Oscar Amulungu <110774590+OscarAmulungu@users.noreply.github.com>
2023-09-01 17:51:59 +0200

    Update HuaweiTest.java

--- HuaweiTest.java
+++ HuaweiTest.java
@@ -1,28 +1,25 @@
 package Company;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 public class HuaweiTest {
 
 	@Test
+    public void testPriceWithNegativeValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Huawei("Huawei D3", -5000.86, "Black", 2022, 6.2);
+        });
+    }
+	@Test
 	public void testSellingPrice() {
-		Huawei huawei = new Huawei("Huawei D3", 4500.86, "Black", "Huawei",2018, 6.2);
-		// Calculate the expected selling price
-        Double expectedSellingPrice = 4500.86 + 4500.86 * 0.3;
-
-        // Assert the expected selling price with the actual result
-        assertEquals(expectedSellingPrice, huawei.SellingPrice(), 0.001);
+		Huawei huawei = new Huawei("Huawei D3", 5000.86, "Black",2018, 6.2);
+        Double expectedSellingPrice = 5000.86 + 5000.86 * 0.3;
+        assertEquals(expectedSellingPrice, huawei.SellingPrice(),0.001);
     }
     @Test
     public void testDiscountPrice() {
-        // Create a Samsung object with specific values
-    	Huawei huawei = new Huawei("Huawei D3", 4500.86, "Black", "Huawei",2018, 6.2);
-
-        // Calculate the expected discount price
-        Double expectedDiscountPrice = (4500.86 + 4500.86 * 0.3) - 200;
-
-        // Assert the expected discount price with the actual result
-        assertEquals(expectedDiscountPrice, huawei.DiscountPrice(), 0.001);
+    	Huawei huawei = new Huawei("Huawei D3", 5000.86, "Black",2018, 6.2);
+        Double expectedDiscountPrice = (5000.86 + 5000.86 * 0.3)- 200;
+        assertEquals(expectedDiscountPrice, huawei.DiscountPrice(),0.001);
     }
 }
