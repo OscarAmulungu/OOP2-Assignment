c747b08aae67a0e1d9670336e01899034ba2f2fa
Oscar Amulungu <110774590+OscarAmulungu@users.noreply.github.com>
2023-09-01 16:43:14 +0200

    Update PhoneShop.java

--- PhoneShop.java
+++ PhoneShop.java
@@ -1,145 +1,229 @@
 package Company;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class PhoneShop {
-	private Device[] dev;
-	private int amount;
-	public PhoneShop(){
-		dev = new Device[1000];
-		amount = 0;
-	}
-	
-	
-	public void AddNewDevice(Device newDevice){
-		if(amount<1000) {
-			dev[amount]= newDevice;
-			++amount;
-		}
-	}
-	
-	public Integer CountHowManySamsungDevices(){
-		int count = 0;
-		for(int i = 0; i<amount; i++) {
-			if(dev[i].GetBrand()==("Samsung")){
-				count++;
-			}
-		}
-		return count;
-	}
-	public Integer CountHowManyIphone13Devices(){
-		int count = 0;
-		for(int i = 0; i<amount; i++) {
-			if(dev[i].GetName()==("IPhone 13")){
-				count++;
-			}
-		}
-		return count;
-	}
-	public Integer CountHowManyHuaweiDevices(){
-		int count = 0;
-		for(int i = 0; i<amount; i++) {
-			if(dev[i].GetBrand()==("Huawei")){
-				count++;
-			}
-		}
-		return count;
-	}
-	public String NameOfMostExpensiveDevice(){
-		double expensive = dev[0].GetBasicPrice();
-		String Name = dev[0].GetName();
-		for(int i = 1; i<amount; i++) {
-			if(dev[i].GetBasicPrice()> expensive) {
-				expensive = dev[i].GetBasicPrice();
-				Name = dev[i].GetName();
-			}
-		}return Name;
-		
-	}
-	public String NameOfCheapestDevice(){
-		double cheapest = dev[0].GetBasicPrice();
-		String Name =dev[0].GetName();
-		for(int i = 1; i<amount; i++) {
-			if(dev[i].GetBasicPrice()< cheapest) {
-				cheapest = dev[i].GetBasicPrice();
-				Name =dev[i].GetName();
-			}
-		}return Name;
-		
-	}
-	public Double TotalPriceofDevices(){
-		double sum = 0;
-		for(int i = 0; i< amount; i++) {
-			sum = sum + dev[i].GetBasicPrice();
-		}return sum;
-	}
-	
-	public Double AveragePrice(){
-		int sum = 0;
-		for(int i =0; i< amount; i++) {
-			sum = (int) (sum + dev[i].GetBasicPrice());
-		}
-		double average = sum/amount;
-		return average;
-	}
-	public Boolean FindIfHuaweiD3WasSold(String string)
-    {
+    private Device[] dev;
+    private int amount;
+
+    public PhoneShop() {
+        dev = new Device[1000];
+        amount = 0;
+    }
+
+    public void AddNewDevice(Device newDevice) throws Exception {
+
+        try {
+            if (amount < 1000) {
+                dev[amount] = newDevice;
+                ++amount;
+            }
+        } catch (Exception e) {
+            e = new Exception();
+            throw e;
+        }
+
+    }
+
+    public Integer CountHowManySamsungDevices() {
+        int count = 0;
+        for (int i = 0; i < amount; i++) {
+            if (dev[i].GetName() == ("Samsung")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Integer HowMany2018Devices() {
+        int count = 0;
+        for (int i = 0; i < amount; i++) {
+            if (dev[i].GetYear() == 2018) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Integer CountHowManyIphone13Devices() {
+        int count = 0;
+        for (int i = 0; i < amount; i++) {
+            if (dev[i].GetName() == ("IPhone 13")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Integer CountHowManyHuaweiDevices() {
+        int count = 0;
+        for (int i = 0; i < amount; i++) {
+            if (dev[i].GetName() == ("Huawei")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String NameOfMostExpensiveDevice() {
+        double expensive = dev[0].GetBasicPrice();
+        String Name = dev[0].GetName();
+        for (int i = 1; i < amount; i++) {
+            if (dev[i].GetBasicPrice() > expensive) {
+                expensive = dev[i].GetBasicPrice();
+                Name = dev[i].GetName();
+            }
+        }
+        return Name;
+
+    }
+
+    public String NameOfCheapestDevice() {
+        double cheapest = dev[0].GetBasicPrice();
+        String Name = dev[0].GetName();
+        for (int i = 1; i < amount; i++) {
+            if (dev[i].GetBasicPrice() < cheapest) {
+                cheapest = dev[i].GetBasicPrice();
+                Name = dev[i].GetName();
+            }
+        }
+        return Name;
+
+    }
+
+    public Double LargestDevice() {
+        Double largest = dev[0].GetDisplaySize();
+        for (int i = 1; i < amount; i++) {
+            if (dev[i].GetDisplaySize() > largest) {
+                largest = dev[i].GetDisplaySize();
+            }
+        }
+        return largest;
+
+    }
+
+    public Double SmallestDevice() {
+        Double smallest = dev[0].GetDisplaySize();
+        for (int i = 1; i < amount; i++) {
+            if (dev[i].GetDisplaySize() < smallest) {
+                smallest = dev[i].GetDisplaySize();
+            }
+        }
+        return smallest;
+
+    }
+
+    public Double AveragePrice() throws Exception {
+        int sum = 0;
+        if (amount == 0) {
+            Exception e = new Exception();
+            throw e;
+
+        } else {
+            for (int i = 0; i < amount; i++) {
+                sum = (int) (sum + dev[i].GetBasicPrice());
+            }
+        }
+        double average = sum / amount;
+        return average;
+    }
+
+    public Double TotalPriceofDevices() {
+        double sum = 0;
+        for (int i = 0; i < amount; i++) {
+            sum = sum + dev[i].GetBasicPrice();
+        }
+        return sum;
+    }
+
+    public Boolean FindIfHuaweiD3WasSold(String string) {
         Boolean c = false;
-        for (int i = 0; i<amount; i++)
-        {
-            if (dev[i].GetName() == "Huawei D3")
-            {
+        for (int i = 0; i < amount; i++) {
+            if (dev[i].GetName() == "Huawei D3") {
                 c = true;
             }
         }
         return c;
     }
 
-	public Boolean ContainsSpecificWhiteDevices(String string){
-		for(int i =0; i<amount; i++) {
-			if("White".equalsIgnoreCase(dev[i].GetColor())) {
-				return true;
-			}
-		}return false;
-	}
-	public Boolean ContainsSpecificBlackDevices(String string){
-		for(int i =0; i<amount; i++) {
-			if("Black".equalsIgnoreCase(dev[i].GetColor())) {
-				return true;
-			}
-		}return false;
-	}
-	public Boolean ContainsSpecificHisenseDevices(String string){
-		for(int i =0; i<amount; i++) {
-			if("Nokia".equalsIgnoreCase(dev[i].GetBrand())) {
-				return true;
-			}
-		}return false;
-	}
-	public Double LargestDevice(){
-		Double largest = dev[0].GetDisplaySize();
-		for(int i = 1; i<amount; i++) {
-			if(dev[i].GetDisplaySize()> largest) {
-				largest = dev[i].GetDisplaySize();
-			}
-		}return largest;
-		
-	}
-	public Double SmallestDevice(){
-		Double smallest = dev[0].GetDisplaySize();
-		for(int i = 1; i<amount; i++) {
-			if(dev[i].GetDisplaySize()< smallest) {
-				smallest = dev[i].GetDisplaySize();
-			}
-		}return smallest;
-		
-	}
-	public Integer HowMany2018Devices(){
-		int count = 0;
-		for(int i = 0; i<amount; i++) {
-			if(dev[i].GetYear()== 2018){
-				count++;
-			}
-		}
-		return count;
-	}
-	
+    public Boolean ContainsSpecificWhiteDevices(String string) {
+        for (int i = 0; i < amount; i++) {
+            if ("White".equalsIgnoreCase(dev[i].GetColor())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean ContainsSpecificBlackDevices(String string) {
+        for (int i = 0; i < amount; i++) {
+            if ("Black".equalsIgnoreCase(dev[i].GetColor())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean ContainsSpecificHisenseDevices(String string) {
+        for (int i = 0; i < amount; i++) {
+            if ("Nokia".equalsIgnoreCase(dev[i].GetName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String DeviceBrandContainUA() throws Exception {
+        String nameUA = "";
+        try {
+            for (int i = 0; i < amount; i++) {
+                if ((dev[i].GetName().contains("ua")) || (dev[i].GetName().contains("UA"))) {
+
+                    nameUA += dev[i].GetName();
+                }
+            }
+        } catch (Exception e) {
+            e = new Exception();
+            throw e;
+        }
+        return nameUA;
+    }
+
+    public void SaveDeviceInfo(String filename) throws Exception {
+        File file = null;
+        PrintWriter DI = null;
+        try {
+            file = new File("C:\\Users\\Admin\\Desktop\\Devices.txt");
+            DI = new PrintWriter(file);
+            for (int i = 0; i < amount; i++) {
+                DI.println(dev[i].toString());
+                DI.println(dev[i].GetName());
+                DI.println(dev[i].GetYear());
+                DI.println(dev[i].GetColor());
+                DI.println(dev[i].GetBasicPrice());
+                
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            DI.close();
+        }
+    }
+
+    public void ReadDeviceInfo(String filename) throws Exception {
+        File file = null;
+        Scanner DI = null;
+        try {
+            file = new File("C:\\Users\\Admin\\Desktop\\Devices.txt");
+             DI = new Scanner(file);
+            while (DI.hasNextLine())
+                System.out.println(DI.nextLine());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            DI.close();
+        }
+    }
+    
 }
