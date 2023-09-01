d85eaaf0f615fd71feb4d617e6a1664c8d27fafa
Oscar Amulungu <110774590+OscarAmulungu@users.noreply.github.com>
2023-09-01 17:53:03 +0200

    Update IPhoneTest.java

--- IPhoneTest.java
+++ IPhoneTest.java
@@ -1,28 +1,25 @@
 package Company;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 public class IPhoneTest {
 
 	@Test
+    public void testPriceWithNegativeValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new IPhone("IPhone 11", -9000.32, "Black", 2022, 6.2);
+        });
+    }
+	@Test
 	public void testSellingPrice() {
-		IPhone iphone = new IPhone("IPhone 11", 9000.86, "Black", "IPhone",2018, 6.2);
-		// Calculate the expected selling price
+		IPhone iphone = new IPhone("IPhone 11", 9000.86, "Black",2018, 6.2);
         Double expectedSellingPrice = 9000.86 + 9000.86 * 0.5;
-
-        // Assert the expected selling price with the actual result
         assertEquals(expectedSellingPrice, iphone.SellingPrice(), 0.001);
     }
     @Test
     public void testDiscountPrice() {
-        // Create a Samsung object with specific values
-    	IPhone iphone = new IPhone("IPhone 11", 9000.86, "Black", "IPhone",2018, 6.2);
-
-        // Calculate the expected discount price
+    	IPhone iphone = new IPhone("IPhone 11", 9000.86, "Black",2018, 6.2);
         Double expectedDiscountPrice = (9000.86 + 9000.86 * 0.5) - 800;
-
-        // Assert the expected discount price with the actual result
-        assertEquals(expectedDiscountPrice, iphone.DiscountPrice(), 0.001);
+        assertEquals(expectedDiscountPrice, iphone.DiscountPrice(),0.001);
     }
 }
