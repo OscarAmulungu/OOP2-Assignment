59befbfa0f27c2907f2b3e580bba36545e1ab063
Oscar Amulungu <110774590+OscarAmulungu@users.noreply.github.com>
2023-09-01 16:41:27 +0200

    Update IPhone.java

--- IPhone.java
+++ IPhone.java
@@ -1,18 +1,21 @@
 package Company;
 
-//Inheritance
 public class IPhone extends Device {
 
-	public IPhone(String nN, Double nP, String nC, String nB, Integer nY, Double nDS) {
-		super(nN, nP , nC, nB, nY, nDS);
-		// TODO Auto-generated constructor stub
+    public IPhone(String nN, Double nP, String nC,  Integer nY, Double nDS) {
+        super(nN, nP , nC, nY, nDS);
+        if(nP <= 0) {
+			throw new IllegalArgumentException("Price must be a positive value");
+		}
 	}
-	@Override
-	public Double SellingPrice() {
-		return BasicPrice + BasicPrice*0.5;
-	}
-	@Override
-	public Double DiscountPrice(){
-		return SellingPrice() - 800;
-	}
+        
+    
+    @Override
+    public Double SellingPrice() {
+        return BasicPrice + BasicPrice*0.5;
+    }
+    @Override
+    public Double DiscountPrice(){
+        return SellingPrice() - 800;
+    }
 }
