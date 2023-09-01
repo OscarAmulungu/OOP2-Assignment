230b0a678706d6cb104d8a980fa174d7cd988f9e
Oscar Amulungu <110774590+OscarAmulungu@users.noreply.github.com>
2023-09-01 17:54:01 +0200

    Update SamsungTest.java

--- SamsungTest.java
+++ SamsungTest.java
@@ -1,28 +1,26 @@
 package Company;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 public class SamsungTest {
 
+
+    @Test
+    public void testPriceWithNegativeValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Samsung("Samsung Galaxy S21", -2555.0, "Black", 2022, 6.2);
+        });
+    }
 	@Test
 	public void testSellingPrice() {
-		Samsung samsung = new Samsung("Samsung Galaxy S10", 10200.45, "Black", "Samsung",2018, 6.1);
-		// Calculate the expected selling price
-        Double expectedSellingPrice = 10200.45 + 10200.45 * 0.4;
-
-        // Assert the expected selling price with the actual result
-        assertEquals(expectedSellingPrice, samsung.SellingPrice(), 0.001);
+		Samsung samsung = new Samsung("Samsung Galaxy S21", 10200.10, "Black",2022, 6.2);
+        Double expectedSellingPrice = 10200.10 + 10200.10 * 0.4;
+        assertEquals(expectedSellingPrice, samsung.SellingPrice(),0.001);
     }
     @Test
     public void testDiscountPrice() {
-        // Create a Samsung object with specific values
-        Samsung samsung = new Samsung("Samsung Galaxy S10", 10200.45, "Black", "Samsung", 2021, 6.1);
-
-        // Calculate the expected discount price
-        Double expectedDiscountPrice = (10200.45 + 10200.45 * 0.4) - 1100;
-
-        // Assert the expected discount price with the actual result
-        assertEquals(expectedDiscountPrice, samsung.DiscountPrice(), 0.001);
+    	Samsung samsung = new Samsung("Samsung Galaxy S21", 10200.10, "Black",2022, 6.2);
+        Double expectedDiscountPrice = (10200.10 + 10200.10 *0.4) - 1100;
+        assertEquals(expectedDiscountPrice, samsung.DiscountPrice(),0.001);
     }
 }
